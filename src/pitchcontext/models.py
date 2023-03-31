bdb488be8164dfd54ece8a1b139d58cefbc5b778
Peter van Kranenburg <peter.van.kranenburg@meertens.knaw.nl>
2023-03-31 02:02:55 +0200

    Implementation of getChord

--- src/pitchcontext/models.py
+++ src/pitchcontext/models.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy.linalg import norm
 from numpy import inf
 from matplotlib import pyplot as plt
+from copy import deepcopy
 
 from .pitchcontext import PitchContext
 from .song import Song
@@ -247,10 +248,13 @@ def computeUnharmonicity(
     dissonance: np.array,
     consonance: np.array,
     beatstrength_treshold: float,
+    lastnoteharmonic: bool = False,
     epsilon: float = 10e-4
 ):
     """Computes for each note the degree to which it is 'unharmonic'.
-    Each note with beatstrength lower than beatstrength_threshold, and dissonant in its context is considered 'unharmonic'.
+    The unharmonicity value is the dissonance minus the consonance of a note in its context, with lower boundary of zero.
+    Each note with beatstrength lower than beatstrength_threshold, and dissonant in its context is considered 'unharmonic' (value: 0.0).
+    If parameter lastnoteharmonic is True, the last note gets unharmonicity value 0.0
 
     Parameters
     ----------
@@ -258,7 +262,15 @@ def computeUnharmonicity(
         Ojbect with song data.
     wpc : PitchContext
         Object with pitch context data
-
+    dissonance : 1D numpy array
+        Dissonance values as returned by computeDissonance
+    consonance : 1D numpy array
+        Consonance values as returned by computeConsonance
+    beatstrength_threshold : float
+        Consider all notes with beatstrength >= beatstrength_threshold as 'harmonic' (value 0.0).
+    lastnoteharmonic : bool
+        If True, always consider the last note of the melody as 'harmonic' (value 0.0).
+        
     Returns
     -------
     numpy array
@@ -274,42 +286,65 @@ def computeUnharmonicity(
             else:
                 unharmonicity[ix] = max ( dissonance[ix] - consonance[ix], 0.0)
                 #unharmonicity[ix] = dissonance[ix] / consonance[ix]
+    #do last note separately
+    if lastnoteharmonic:
+        unharmonicity[-1] = 0.0  # e.g. NLB123866_01
     return unharmonicity
 
 def getChords(pitchcontextvector):
     #find out whether pitches could be arranged as series of thirds
     
-    #series of thirds in base40:
-    #[0-4, 11-16, 23-27, 34-39]
-    chordmask = np.ones(40) * -1.0
-    chordmask[2] = 1.0 # root
-    chordmask[11:17] = 1.0 #dim/min/maj/aug third
-    chordmask[23:28] = 1.0 #dim/pure/aug fifth
-    #chordmask[34:40] = 1.0 #dim/min/maj/aug seventh
+    epsilon = 10e-4
 
-    print(chordmask)
+    chordmask_dim = np.zeros(40)
+    chordmask_min = np.zeros(40)
+    chordmask_maj = np.zeros(40)
+    chordmask_dom = np.zeros(40)
+    chordmask_minseventh = np.zeros(40)
+    np.put(chordmask_dim, [0, 11, 22], 1.0)
+    np.put(chordmask_min, [0, 11, 23], 1.0)
+    np.put(chordmask_maj, [0, 12, 23], 1.0)
+    np.put(chordmask_dom, [0, 12, 23, 34], 1.0)
+    np.put(chordmask_minseventh, [34], 1.0) #used for check presence seventh in dom chord
 
-    #get a value for every rotation of chordmask
-    score_pre = np.zeros(40)
-    score_post = np.zeros(40)
-    score_all = np.zeros(40)
+    masks = np.stack([chordmask_dim, chordmask_min, chordmask_maj, chordmask_dom])
+    numchordmasks = masks.shape[0]
+
+    #only take natural tones, and one b or one # as root
+    valid_shifts = [1, 2, 3, 7, 8, 9, 13, 14, 15, 18, 19, 20, 24, 25, 26, 30, 31, 32, 36, 37, 38]
+
+    #get a value for every rotation of the chordmasks
+    score_pre = np.zeros((40, numchordmasks))
+    score_post = np.zeros((40, numchordmasks))
+    score_all = np.zeros((40, numchordmasks))
+    strength_pre = np.zeros((40, numchordmasks))
+    strength_post = np.zeros((40, numchordmasks))
+    strength_all = np.zeros((40, numchordmasks))
     for shift in range(40):
-        chordmask_shift = np.roll(chordmask, shift)
-        score_pre[shift]  = np.sum(np.multiply(pitchcontextvector[:40],chordmask_shift))
-        score_post[shift] = np.sum(np.multiply(pitchcontextvector[40:],chordmask_shift))
-        score_all[shift] = np.sum(np.multiply(pitchcontextvector[:40]+pitchcontextvector[40:],chordmask_shift))
+        if not shift in valid_shifts:
+            continue
 
-    print("pre:")
-    for ix in np.where(pitchcontextvector[:40]>0.)[0]:
-        print(ix, base40[ix], pitchcontextvector[ix])
+        chordmask_shift = np.roll(masks, shift, axis=1)
+        chordmask_minseventh_shift = np.roll(chordmask_minseventh, shift)
+                
+        score_pre[shift]  = np.sum(np.multiply(pitchcontextvector[:40],chordmask_shift), axis=1)
+        strength_pre[shift] = score_pre[shift] / np.sum(pitchcontextvector[:40])
+        score_post[shift] = np.sum(np.multiply(pitchcontextvector[40:],chordmask_shift), axis=1)
+        strength_post[shift] = score_post[shift] / np.sum(pitchcontextvector[40:])
+        score_all[shift] = np.sum(np.multiply(pitchcontextvector[:40]+pitchcontextvector[40:],chordmask_shift), axis=1)
+        strength_all[shift] = score_all[shift] / np.sum(pitchcontextvector)
 
-    print("post:")
-    for ix in np.where(pitchcontextvector[40:]>0.)[0]:
-        print(ix, base40[ix], pitchcontextvector[ix+40])
-    
-    for shift in range(40):
-        print(base40[shift], ': ', score_pre[shift])
+        #if seventh in dom chord is not present -> erase dom chord
+        if np.sum(np.multiply(chordmask_minseventh_shift,pitchcontextvector[:40])) < epsilon:
+            score_pre[shift][3] = 0.0
+            strength_pre[shift][3] = 0.0
+        if np.sum(np.multiply(chordmask_minseventh_shift,pitchcontextvector[40:])) < epsilon:
+            score_post[shift][3] = 0.0
+            strength_post[shift][3] = 0.0
+        if np.sum(np.multiply(chordmask_minseventh_shift,pitchcontextvector[:40]+pitchcontextvector[:40])) < epsilon:
+            score_all[shift][3] = 0.0
+            strength_all[shift][3] = 0.0
 
-    return score_pre, score_post, score_all
+    return score_pre, score_post, score_all, strength_pre, strength_post, strength_all
 
 
